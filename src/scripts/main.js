35d6ee1af8990327c5aa0fdcc92dce68b4b72f33
inirafli <raplirayhann@gmail.com>
2023-09-15 19:31:42 +0700

    layout for main content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,6 +16,65 @@ const main = () => {
     main.addEventListener('click', () => {
         drawer.classList.remove('open')
     })
+
+    fetch('/data/DATA.json')
+        .then(response => response.json())
+        .then(data => {
+            createRestaurantList(data.restaurants)
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        })
+}
+
+const createRestaurantList = restaurants => {
+    const restaurantList = document.querySelector('.restaurant-list')
+
+    restaurants.forEach(restaurant => {
+        const card = createRestaurantCard(restaurant)
+        restaurantList.appendChild(card)
+    })
+}
+
+const createRestaurantCard = restaurant => {
+    const card = document.createElement('article')
+    card.classList.add('card')
+
+    const img = document.createElement('img')
+    img.src = restaurant.pictureId
+    img.alt = restaurant.name
+    card.appendChild(img)
+
+    const restaurantInfo = document.createElement('div')
+    restaurantInfo.classList.add('restaurant-info')
+
+    const restaurantLabel = document.createElement('div')
+    restaurantLabel.classList.add('restaurant-label')
+    const city = document.createElement('p')
+    city.classList.add('restaurant-city')
+    city.textContent = `Kota ${restaurant.city}`
+    restaurantLabel.appendChild(city)
+
+    const name = document.createElement('h3')
+    name.classList.add('restaurant-name')
+    name.textContent = restaurant.name
+
+    const rating = document.createElement('p')
+    rating.classList.add('restaurant-rating')
+    rating.textContent = `Rating: ${restaurant.rating}`
+
+    const description = document.createElement('p')
+    description.classList.add('restaurant-description')
+    description.textContent = restaurant.description
+
+    restaurantInfo.appendChild(restaurantLabel)
+    restaurantInfo.appendChild(name)
+    restaurantInfo.appendChild(rating)
+    restaurantInfo.appendChild(description)
+
+    card.appendChild(restaurantInfo)
+
+    return card
 }
 
 export default main
